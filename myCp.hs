2c5311cdf32b3e29b79b564577f559090c30edc6
Penny Chase <mpchase@mac.com>
2021-01-01 18:30:54 -0500

    myCp.hs a simple version of cp

---
+++ myCp.hs
@@ -0,0 +1,34 @@
+
+import System.Environment ( getArgs )
+import System.Exit
+import System.IO
+import Data.Text ( Text )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+-- Argument handling
+
+-- Simple parser
+parse :: [String] -> IO ()
+parse [] = usage >> exit
+parse [_] = usage >> exit
+parse [src, dest] = if src == dest
+                    then usage >> exit
+                    else copy src dest
+
+-- Print usage on error
+usage :: IO ()
+usage = putStrLn "usage: myCp source destination"
+
+-- Exit
+exit :: IO ()
+exit = exitWith (ExitFailure 1)
+
+-- copy file
+copy :: String -> String -> IO ()
+copy srcFileName destFileName = do
+    contents <- TIO.readFile srcFileName
+    TIO.writeFile destFileName contents
+
+main :: IO ()
+main = getArgs >>= parse
