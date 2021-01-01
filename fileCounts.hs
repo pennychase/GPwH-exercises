a7c528b6dfe2762dc9373cfd86a5ecdab2f7cbed
Penny Chase <mpchase@mac.com>
2021-01-01 17:53:08 -0500

    filelCounts.hs changing position of hClose to after read is used

--- fileCounts.hs
+++ fileCounts.hs
@@ -12,15 +12,17 @@ countsText :: (Int, Int, Int) -> String
 countsText (cc, wc, lc) =
     mconcat ["chars: ", show cc, " words: ", show wc, " lines: ", show lc]
 
--- using explicit handle. hClose closes the handle before input is used, so hGetContents 
--- actually tries the read in the following expression
+-- using explicit handle. Move hClose after the hGetContents is used. In the book, it was after
+-- the "let summary = ...", but that still failed. Maybe because summary wan't used, so the read
+-- still wasn't forced? It works after the appendFile. But still doesn't work with stats.dat, since
+-- closing file after trying to write to it.
 main :: IO ()
 main = do
     args <- getArgs
     let filename = head args
     file <- openFile filename ReadMode
     input <- hGetContents file
-    hClose file
     let summary = (countsText . getCounts) input
     appendFile "stats.dat" (mconcat [filename, " ", summary, "\n"])
+    hClose file
     putStrLn summary
