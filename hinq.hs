b0f71bbab1b9b66780637e9397694657c64737fc
Penny Chase <mpchase@mac.com>
2021-09-19 14:27:05 -0400

    hinq.hs modified: generalized clauses to monads and created HINQ type to run quesries

--- hinq.hs
+++ hinq.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import Control.Monad
 
 
@@ -28,6 +29,89 @@ data Course = Course
     , teacher :: Int
     } deriving Show
 
+data Enrollment = Enrollment
+    { student :: Int
+    , course :: Int
+    } deriving Show
+
+data HINQ m a b = HINQ (m a -> m b) (m a) (m a -> m a) -- _select _join/data _where
+                | HINQ_ (m a -> m b) (m a)             -- no _whhere clause
+
+runHINQ :: (Monad m, Alternative m) => HINQ m a b -> m b
+runHINQ (HINQ sClause jClause wClause) = _hinq sClause jClause wClause
+runHINQ (HINQ_ sClause jClause) = _hinq sClause jClause (_where (\_ -> True)) 
+
+-- _select, _join, _where rewritten to take monads instead of lists
+-- To support using the HINQ type for representing queries
+
+-- Select property from values
+_select :: Monad m => (a -> b) -> m a -> m b
+_select prop vals = do
+    val <- vals
+    return (prop val)
+
+-- Filter values
+_where :: (Monad m, Alternative m) => (a ->Bool) -> m a -> m a
+_where test vals = do
+    val <- vals
+    guard (test val)
+    return val
+
+-- Inner Join
+_join :: (Monad m, Alternative m, Eq c) => m a -> m b -> (a -> c) -> (b -> c) -> m (a,b)
+_join data1 data2 prop1 prop2 = do
+    d1 <- data1
+    d2 <- data2
+    let dpairs = (d1, d2)
+    guard ((prop1 (fst dpairs)) == (prop2 (snd dpairs)))
+    return dpairs
+
+{-
+-- Origial versions using lists
+-- Select property from values
+_select :: (a -> b) -> [a] -> [b]
+_select prop vals = do
+    val <- vals
+    return (prop val)
+
+-- Filter values
+_where :: (a ->Bool) -> [a] -> [a]
+_where test vals = do
+    val <- vals
+    guard (test val)
+    return val
+
+-- Inner Join
+_join :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a,b)]
+_join data1 data2 prop1 prop2 = do
+    d1 <- data1
+    d2 <- data2
+    let dpairs = (d1, d2)
+    guard ((prop1 (fst dpairs)) == (prop2 (snd dpairs)))
+    return dpairs
+-}
+
+-- Run a query using SQL order (select <elements> from <data> where <tests>)
+-- Using lambda
+_hinq selectQuery joinQuery whereQuery =
+    (\joinData -> 
+        (\whereResult ->
+            selectQuery whereResult)
+        (whereQuery joinData)
+    ) joinQuery
+
+-- Using let
+_hinq' selectQuery joinQuery whereQuery =
+    let
+        joinData = joinQuery
+        whereData = whereQuery joinData
+    in  selectQuery whereData
+
+-- Some properties to use in testing
+startsWith :: Char -> String -> Bool
+startsWith c str = c == head str
+
+-- Example Data
 students :: [Student]
 students = 
     [ Student 1 Senior (Name "Audre" "Lorde")
@@ -50,45 +134,18 @@ courses = [ Course 101 "French" 100
           , Course 202 "English" 220
           ]
 
--- Select property from values
-_select :: (a -> b) -> [a] -> [b]
-_select prop vals = do
-    val <- vals
-    return (prop val)
+enrollments :: [Enrollment]
+enrollments = [ (Enrollment 1 101)
+              , (Enrollment 2 101)
+              , (Enrollment 2 201)
+              , (Enrollment 3 101)
+              , (Enrollment 4 201)
+              , (Enrollment 4 101)
+              , (Enrollment 5 101)
+              , (Enrollment 6 201)
+              ]
 
--- Filter values
-_where :: (a ->Bool) -> [a] -> [a]
-_where test vals = do
-    val <- vals
-    guard (test val)
-    return val
-
--- Inner Join
-_join :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a,b)]
-_join data1 data2 prop1 prop2 = do
-    d1 <- data1
-    d2 <- data2
-    let dpairs = (d1, d2)
-    guard ((prop1 (fst dpairs)) == (prop2 (snd dpairs)))
-    return dpairs
-
--- Run a query using SQL order (select <elements> from <data> where <tests>)
-_hinq selectQuery joinQuery whereQuery =
-    (\joinData -> 
-        (\whereResult ->
-            selectQuery whereResult)
-        (whereQuery joinData)
-    ) joinQuery
-
-_hinq' selectQuery joinQuery whereQuery =
-    let
-        joinData = joinQuery
-        whereData = whereQuery joinData
-    in  selectQuery whereData
-
--- Some properties to use in testing
-startsWith :: Char -> String -> Bool
-startsWith c str = c == head str
+-- Example queries
 
 -- Query without where using \_ -> True)
 finalResult :: [Name]
@@ -100,3 +157,51 @@ teacherFirstName :: [String ]
 teacherFirstName = _hinq (_select firstName)
                          finalResult
                          (_where (\_ -> True))
+
+-- Using HINQ with lists
+query1 :: HINQ [] (Teacher, Course) Name
+query1 = HINQ (_select (teacherName . fst))
+              (_join teachers courses teacherId teacher)
+              (_where ((== "English") . courseTitle . snd))
+
+query2 :: HINQ [] Teacher Name
+query2 = HINQ_ (_select teacherName)
+              teachers
+
+-- Using HINQ with Maybe
+possibleTeacher :: Maybe Teacher
+possibleTeacher = Just (head teachers)
+
+possibleCourse :: Maybe Course
+possibleCourse = Just (head courses)
+
+missingCourse :: Maybe Course
+missingCourse = Nothing
+
+-- Can use missingCourse to demonstrate what happens with missing data
+-- Change _where clause to (== "English") to see what happens when _where fails
+maybeQuery1 :: HINQ Maybe (Teacher, Course) Name
+maybeQuery1 = HINQ (_select (teacherName . fst))
+              (_join possibleTeacher possibleCourse teacherId teacher)
+              (_where ((== "French") . courseTitle . snd))
+
+-- Enrollments and joins
+studentEnrollmentsQ = HINQ_ (_select (\(st, en) ->
+                                       (studentName st, course en)))
+                            (_join students enrollments studentId student)
+
+studentEnrollments = runHINQ studentEnrollmentsQ
+
+studentCoursesQ = HINQ_ (_select (\(p, c) -> 
+                                   (fst p, courseTitle c)))
+                        (_join studentEnrollments courses snd courseId)
+
+getEnrollments :: String -> [Name]
+getEnrollments courseName = runHINQ courseQuery
+    where
+        courseQuery = HINQ  (_select (fst . fst))
+                            (_join studentEnrollments courses snd courseId)
+                            (_where ((== courseName) . courseTitle . snd))
+
+
+
