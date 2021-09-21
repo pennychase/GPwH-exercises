02babd50916b616032564b67fb1d6e7d70912592
Penny Chase <mpchase@mac.com>
2021-09-21 19:53:24 -0400

    Added CLI using Either for error handling

--- primes/app/Main.hs
+++ primes/app/Main.hs
@@ -1,6 +1,34 @@
 module Main where
 
+import System.Environment
 import Primes
 
+-- Extend the capstone of Unit 6 by adding a CLI
+-- But use the error handling from Unit 7 whichh uses Either
+
+primeTest :: IO ()
+primeTest = do
+    print "Enter a number to test for primaility: "
+    n <- read <$> getLine
+    let result = isPrime n
+    case result of
+        (Right True) -> print "It's prime"
+        (Right False) -> print "It's composite"
+        (Left primeError) -> print primeError
+
+factorIntoPrimes :: IO ()
+factorIntoPrimes = do
+    print "Ener a number to factor into primes: "
+    n <- read <$> getLine
+    let result = primeFactors n
+    case result of
+        (Left primeError) -> print primeError
+        (Right factors) -> print $ "The factors are: " ++ show factors
+              
 main :: IO ()
-main = return ()
+main = do
+    args <- getArgs
+    case (head args) of
+        "-p" -> primeTest
+        "-f" -> factorIntoPrimes
+        otherwise -> putStrLn "Usage: primes-exe [-p | -f]"
